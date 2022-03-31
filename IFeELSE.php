568d21239d4c144f7cbd9ea90ece033c6e78fd69
devGBR <gfcontroladoriageral@gmail.com>
2022-03-31 10:25:57 -0300

    IF e ELSE em uma linha

--- IFeELSE.php
+++ IFeELSE.php
@@ -1,18 +1,21 @@
 <?php
- $salario = 960; //R$
- $tempo_de_casa = 1; // meses
- $reclamacoes = false; //boolean
+//  $salario = 960; //R$
+//  $tempo_de_casa = 1; // meses
+//  $reclamacoes = false; //boolean
 
- if($salario > 900){
-     if($tempo_de_casa >= 3){
-         if($reclamacoes != TRUE){
+//  if($salario > 900){
+//      if($tempo_de_casa >= 3){
+//          if($reclamacoes != TRUE){
 
-         }
-     }
- };
- if (($salario > 900) and ($tempo_de_casa >= 3) and ($reclamacoes != TRUE) ){
-     echo 'Parabéns será promovido';
- }
- else{
-     echo 'houve insuficiencia em relação a algumas infomações, tente novamente daqui um mes!';
- };
+//          }
+//      }
+//  };
+//  if (($salario > 900) and ($tempo_de_casa >= 3) and ($reclamacoes != TRUE) ){
+//      echo 'Parabéns será promovido';
+//  }
+//  else{
+//      echo 'houve insuficiencia em relação a algumas infomações, tente novamente daqui um mes!';
+//  };
+
+// $resultado = ($salario > 1800) ? 'Pode financiar um carro' : 'Não ha financiamento para o senhor.';
+// echo $resultado;
