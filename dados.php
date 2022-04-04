86d12bdff7258f958f953a493ba7ae69bc71e192
devGBR <gfcontroladoriageral@gmail.com>
2022-04-04 11:52:04 -0300

    banco de dados

--- dados.php
+++ dados.php
@@ -1,12 +1,24 @@
 <?php
     $nome = 'Gabriel';
-    $nomedomeio = 'Felipe';
-    $sobrenome = 'Oliveira';
-    $data_nasc = 31/05/2003;
-    $email = 'gfcontroladoriageral@gmail.com';
+    $espaço = " ";
+    $nomedomeio = "Felipe";
+    $sobrenome = "Oliveira";
+    $data_nasc = "\n31/05/2003";
+    $email = "\ngfcontroladoriageral@gmail.com\n";
+
+    echo $nome ,$espaço , $nomedomeio, $espaço , $sobrenome; 
+
+    $gmail = strtoupper($email);
+     echo $gmail;
 
     $nomecompleto = array($nome . $nomedomeio . $sobrenome);
      var_dump($nomecompleto);
+
+    
+
+     $data = str_replace('/','-',$data_nasc);
+     echo $data;
+    
 
     //  require 'calcularpeso.php';
     //  echo "IMC: ", calculo_IMC(53, 1.80);
